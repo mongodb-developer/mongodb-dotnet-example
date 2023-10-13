a7bc2cfe4a18d7f90d1388c81e5c6a6a4d4f2165
Luce Carter <luce@lucyscarter.co.uk>
2023-10-13 15:37:59 +0100

    Enable Swagger regardless of runtime environment

--- Startup.cs
+++ Startup.cs
@@ -48,9 +48,12 @@ namespace mongodb_dotnet_example
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "mongodb_dotnet_example v1"));
+                
+                
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "mongodb_dotnet_example v1"));
 
             app.UseHttpsRedirection();
 
